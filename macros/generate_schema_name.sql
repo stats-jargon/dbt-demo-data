bdc39f5c33c9d569777c1d92d2d731319f9cfc66
Nathanial Watanabe <nate@df-insights.com>
2026-03-11 12:20:13 +0000

    Updated env_type and setup of .env variable

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,11 +1,11 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-  {%- set is_dev = target.name in ['dev', 'default'] -%}
+  {%- set env_type = env_var('DBT_ENV_TYPE', 'DEV') | upper -%}
 
-  {%- if is_dev -%}
+  {%- if custom_schema_name is none -%}
     {{ target.schema }}
-  {%- elif custom_schema_name is none -%}
-    {{ target.schema }}
-  {%- else -%}
+  {%- elif env_type == 'PROD' -%}
     {{ custom_schema_name | trim }}
+  {%- else -%}
+    {{ target.schema }}
   {%- endif -%}
 {%- endmacro %}
