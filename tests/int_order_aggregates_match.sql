8b8ff70fc78501114cd4eea1853f497a8ffd961c
Nathanial Watanabe <nate@df-insights.com>
2026-03-14 02:18:14 +0000

    Updated test

--- tests/int_order_aggregates_match.sql
+++ tests/int_order_aggregates_match.sql
@@ -3,14 +3,16 @@ WITH
     inc AS (
         SELECT
             COUNT(*) AS row_count,
-            COUNT(DISTINCT order_id) AS pk_count
+            COUNT(DISTINCT order_id) AS pk_count,
+            COUNT(DISTINCT CASE WHEN status = 'cancelled' THEN order_id END) AS cancelled_count
         from {{ ref('int_orders') }}
     ),
 
     src AS (
         SELECT
             COUNT(*) AS row_count,
-            COUNT(DISTINCT order_id) AS pk_count
+            COUNT(DISTINCT order_id) AS pk_count,
+            COUNT(DISTINCT CASE WHEN LOWER(status) = 'cancelled' THEN order_id END) AS cancelled_count
         FROM {{ source("src_thelook_orders", "orders") }}
     )
 
@@ -20,3 +22,4 @@ CROSS JOIN src
 WHERE
     inc.row_COUNT != src.row_COUNT
     OR inc.pk_COUNT != src.pk_COUNT
+    OR inc.cancelled_count != src.cancelled_count
