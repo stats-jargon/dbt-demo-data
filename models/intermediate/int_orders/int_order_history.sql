8040e24107402020f0f9c5f5c2afb6d47576a9b1
Nathanial Watanabe <nate@df-insights.com>
2026-03-14 00:58:01 +0000

    Finalize increment_order_history logic

--- models/intermediate/int_orders/int_order_history.sql
+++ models/intermediate/int_orders/int_order_history.sql
@@ -1,37 +1,37 @@
 {% set order_history_key_construct %}
-    {{dbt_utils.generate_surrogate_key(
+    {{ dbt_utils.generate_surrogate_key(
         [
-            'order_id',
-            'status',
-            'created_at'
+            'src.order_id',
+            'src.status',
+            'src.created_at'
         ]
-    )}}
+    ) }}
 {% endset %}
 
 
 WITH
-    increment_orders AS (
+    increment_order_history AS (
         SELECT
             {{ order_history_key_construct }} AS order_history_id,
-            *,
-            CURRENT_TIMESTAMP()               AS order_history_loaded_at
-        FROM {{ source("src_thelook_orders", "orders") }} 
-        {% if is_incremental %}
-        WHERE
-            CURRENT_TIMESTAMP() > (SELECT MAX(order_history_loaded_at) FROM {{ this }})
+            order_id,
+            user_id,
+            LOWER(status) AS status,
+            LOWER(gender) AS gender,
+            created_at AS created_at_ts,
+            returned_at AS returned_at_ts,
+            shipped_at AS shipped_at_ts,
+            delivered_at AS delivered_at_ts,
+            num_of_item AS number_of_items,
+            CURRENT_TIMESTAMP() AS order_history_loaded_at
+        FROM {{ source("src_thelook_orders", "orders") }} src
+        {% if is_incremental() %}
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM {{ this }} AS existing
+            WHERE existing.order_history_id = {{ order_history_key_construct }}
+        )
         {% endif %}
-    ) 
+    )
 
-SELECT
-    order_history_id,
-    order_id,
-    user_id,
-    LOWER(status)           AS status,
-    LOWER(gender)           AS gender,
-    created_at              AS created_at_ts,
-    returned_at             AS returned_at_ts,
-    shipped_at              AS shipped_at_ts,
-    delivered_at            AS delivered_at_ts,
-    num_of_item             AS number_of_items,
-    order_history_loaded_at
-FROM increment_orders
+SELECT *
+FROM increment_order_history
